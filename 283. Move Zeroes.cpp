2e7230315a5db0e17bc16374bb094020149d534f
AnupRijal <154118700+AnupRijal@users.noreply.github.com>
2024-05-31 23:09:19 -0500

    Add files via upload

---
+++ 283. Move Zeroes.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int lastNonZeroFoundAt = 0;  // This variable keeps track of the position to place the next non-zero element.
+        
+        // First pass: Move all non-zero elements to the beginning of the array.
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] != 0) {
+                nums[lastNonZeroFoundAt++] = nums[i];
+            }
+        }
+        
+        // Second pass: Fill the remaining positions with zeros.
+        for (int i = lastNonZeroFoundAt; i < nums.size(); i++) {
+            nums[i] = 0;
+        }
+    }
+};
